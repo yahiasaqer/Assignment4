a68604ababbce4eba5bc06854604a27dc9f3119e
yahiasaqer <yahiasaqer@gmail.com>
2020-04-25 15:43:22 +0300

    Add files via upload

---
+++ Assignment4/Q1/TableViewController.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter4.Question1;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.FlowPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author hp
+ */
+public class TableViewController implements Initializable {
+
+    @FXML
+    private TextField textFiledStudentId;
+    @FXML
+    private TextField textFiledCourseId;
+    @FXML
+    private TextField textFiledSemester;
+    @FXML
+    private TableColumn<Student, Integer> tcStudentId;
+    @FXML
+    private TableColumn<Student, Integer> tcCourseId;
+    @FXML
+    private TableColumn<Student, String> tcSemester;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button studentPage;
+    @FXML
+    private TableView<?> registrationTableView;
+    @FXML
+    private FlowPane rootPane;
+    
+    Statement statement;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection
+                    = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/student?serverTimezone=UTC",
+                            "root", "");
+            this.statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+        tcStudentId.setCellValueFactory(new PropertyValueFactory("studentId"));
+        tcCourseId.setCellValueFactory(new PropertyValueFactory("courseId"));
+        tcSemester.setCellValueFactory(new PropertyValueFactory("semester"));
+
+    }
+
+    @FXML
+    private void addButtonHandle(ActionEvent event) throws SQLException {
+        Integer studentId = Integer.parseInt(textFiledStudentId.getText());
+        Integer courseId = Integer.parseInt(textFiledCourseId.getText());
+        String semester = textFiledSemester.getText();
+        String sql = "insert into registration values(" + studentId + ", " + courseId + ", '" + semester + "')";
+        this.statement.executeUpdate(sql);
+        clearFields();
+
+    }
+
+    @FXML
+    private void studentPageHandle(ActionEvent event) throws IOException {
+        FlowPane flowPane = FXMLLoader.load(getClass().getResource("StudentTableView.fxml"));
+        rootPane.getChildren().setAll(flowPane);
+    }
+
+
+    private void clearFields() {
+        textFiledStudentId.setText("");
+        textFiledCourseId.setText("");
+        textFiledSemester.setText("");
+    }
+}
